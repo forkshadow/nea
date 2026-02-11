66d5e32db758ea11940423d583f315444fa0d28a
forkshadow <michel.dutilleux@gmail.com>
2026-02-11 19:00:05 +0100

    Update qa.js

--- qa.js
+++ qa.js
@@ -1104,7 +1104,7 @@ window.QA_BASE = [
   a: "D’accord. Dis-moi ce qui te bloque: une phrase, une idée, ou un passage précis ?",
   tags: ["interaction","aide","clarification","nea"],
   updatedAt: "2026-02-11T00:00:00.000Z"
-}
+},
 
 
 
